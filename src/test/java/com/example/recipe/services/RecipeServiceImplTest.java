6d76ecb5cdc12bc32dce0174d0af953de1d1a4a4
Akhil-Janardhanan <akhilkjanchery@gmail.com>
2018-07-05 14:00:21 +0530

    Created a test class for RecipeServiceImpl using Mokito.

---
+++ src/test/java/com/example/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.example.recipe.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.example.recipe.domain.Recipe;
+import com.example.recipe.repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+
+	RecipeServiceImpl recipeService;
+	
+	@Mock
+	RecipeRepository recipeRepository;
+	
+	@Before
+	public void setUp()throws Exception {
+		
+		MockitoAnnotations.initMocks(this);
+		
+		recipeService = new RecipeServiceImpl(recipeRepository);
+	}
+	
+	@Test
+	public void getRecipes()throws Exception{
+		
+		Recipe recipe = new Recipe();
+		HashSet<Recipe> recipeData = new HashSet<>();
+		recipeData.add(recipe);
+		
+		when(recipeService.getRecipes()).thenReturn(recipeData);
+		
+		Set<Recipe> recipes = recipeService.getRecipes();
+		
+		assertEquals(recipes.size(), 1);
+		verify(recipeRepository, times(1)).findAll();
+	}
+}
